7f1718a2eb60b40799f7dc431e528059581d2759
sowmik-sec <sowmik.sec@gmail.com>
2024-11-25 19:04:44 +0600

    create zod validation for auth

--- src/app/modules/auth/auth.interface.ts
+++ src/app/modules/auth/auth.interface.ts
@@ -2,3 +2,12 @@ export type IAuth = {
   phoneNumber: string;
   password: string;
 };
+
+export type ILoginUserResponse = {
+  accessToken: string;
+  refreshToken?: string;
+};
+
+export type IRefreshTokenResponse = {
+  accessToken: string;
+};
