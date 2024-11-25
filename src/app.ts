969dfee2897f935f8bdda1efa3222ccd9d5e766b
sowmik-sec <sowmik.sec@gmail.com>
2024-11-25 20:31:01 +0600

    add cookie parser middleware in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import cors from 'cors';
 import express, { Application } from 'express';
 import routers from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
