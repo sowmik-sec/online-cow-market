1e62fc3ca2145d0129fd4a126fa02ace8716a5c9
sowmik-sec <sowmik.sec@gmail.com>
2024-11-25 12:22:34 +0600

    create static functions for user schema

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -14,7 +14,17 @@ export type IUser = {
   income: number;
 };
 
-export type UserModel = Model<IUser, Record<string, unknown>>;
+export type UserModel = {
+  isUserExist(
+    phoneNumber: string,
+  ): Promise<Pick<IUser, 'phoneNumber' | 'password' | 'role'>>;
+  isPasswordMatched(
+    givenPassword: string,
+    savedPassword: string,
+  ): Promise<boolean>;
+} & Model<IUser>;
+
+// export type UserModel = Model<IUser, Record<string, unknown>>;
 
 export type IUserFilters = {
   searchTerm?: string;
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,6 +1,8 @@
 import { model, Schema } from 'mongoose';
 import { IUser, UserModel } from './user.interface';
 import { role } from './user.constant';
+import config from '../../../config';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<IUser, UserModel>(
   {
@@ -51,5 +53,30 @@ const userSchema = new Schema<IUser, UserModel>(
     },
   },
 );
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+userSchema.statics.isUserExist = async function (
+  phoneNumber: string,
+): Promise<IUser | null> {
+  return User.findOne(
+    { phoneNumber },
+    { phoneNumber: 1, password: 1, role: 1 },
+  );
+};
+
+userSchema.statics.isPasswordMatched = async function (
+  givenPassword: string,
+  savedPassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(givenPassword, savedPassword);
+};
 
 export const User = model<IUser, UserModel>('User', userSchema);
