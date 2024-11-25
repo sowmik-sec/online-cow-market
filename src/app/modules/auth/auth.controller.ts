babc688e4f83fd9ec78b258c4527c37df7d42cb6
sowmik-sec <sowmik.sec@gmail.com>
2024-11-25 19:43:27 +0600

    set refresh token to browser cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,15 +3,23 @@ import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import { StatusCodes } from 'http-status-codes';
 import { AuthService } from './auth.service';
+import config from '../../../config';
+import { ILoginUserResponse } from './auth.interface';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...userData } = req.body;
   const result = await AuthService.loginUser(userData);
-  sendResponse(res, {
+  const { refreshToken, ...other } = result;
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+  sendResponse<ILoginUserResponse>(res, {
     success: true,
     statusCode: StatusCodes.OK,
     message: 'Admin created successfully',
-    data: result,
+    data: other,
   });
 });
 
